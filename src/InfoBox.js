e52a07717da686aa4c5482d8bb5102b172673caa
subashpenneru <subashpenneru@gmail.com>
2020-08-28 16:19:31 +0530

    infobox css changed

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
 
-import { casesTypeColors } from './util';
 import './InfoBox.css';
 
 const InfoBox = ({ active, casesType, title, cases, total, ...props }) => {
-  let colorObj;
-
-  if (active) {
-    colorObj = {
-      borderTop: `10px solid ${casesTypeColors[casesType].hex}`,
-    };
-  }
   return (
-    <Card onClick={props.onClick} className='infoBox' style={colorObj}>
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && 'infoBox__border--' + casesType}`}>
       <CardContent>
         <Typography className='infoBox__title' color='textSecondary'>
           {title}
         </Typography>
-        <h2 className={`infoBox__${casesType}`}>{cases}</h2>
+        <h2 className={`infoBox__cases infoBox--${casesType}`}>{cases}</h2>
         <Typography className='infoBox__total' color='textSecondary'>
           {total}
         </Typography>
