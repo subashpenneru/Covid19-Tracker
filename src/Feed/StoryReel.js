4122064263ccc752f0f624e37c36ecaae888ade7
subashpenneru <subashpenneru@gmail.com>
2020-08-29 12:24:00 +0530

    Squashed commit of the following:
    commit 7d1f123b67e77f352b18bb3415704706244cc61a
    Author: subashpenneru <subashpenneru@gmail.com>
    Date:   Sat Aug 29 12:19:25 2020 +0530
    
        image urls modified
    
    commit 068766e48ed65444b15a4da64bd44344799a2bbb
    Author: subashpenneru <subashpenneru@gmail.com>
    Date:   Sat Aug 29 12:07:33 2020 +0530
    
        deploying to firebase

--- src/Feed/StoryReel.js
+++ src/Feed/StoryReel.js
@@ -7,7 +7,7 @@ const StoryReel = () => {
   return (
     <div className='storyReel'>
       <Story
-        image='https://static.toiimg.com/thumb/72975551.cms?width=680&height=512&imgsize=881753'
+        image='https://scontent.fvga2-1.fna.fbcdn.net/v/t1.0-9/p960x960/118602752_3426128574116124_3504035787252750026_o.jpg?_nc_cat=109&_nc_sid=5b7eaf&_nc_ohc=N4_w-MBwvzsAX8Bw8q3&_nc_ht=scontent.fvga2-1.fna&tp=6&oh=16e046a99e6983f9b20acf7eef73d74f&oe=5F6F5F55'
         profileSrc='http://www.gravatar.com/avatar/e1417e55e634ab81b46a2b75ea6d3793?s=200&r=pg&default=mm'
         title='subash penneru'
       />
@@ -17,7 +17,7 @@ const StoryReel = () => {
         title='subash penneru'
       />
       <Story
-        image='https://static.toiimg.com/thumb/72975551.cms?width=680&height=512&imgsize=881753'
+        image='https://scontent.fvga2-1.fna.fbcdn.net/v/t1.0-9/p960x960/118602752_3426128574116124_3504035787252750026_o.jpg?_nc_cat=109&_nc_sid=5b7eaf&_nc_ohc=N4_w-MBwvzsAX8Bw8q3&_nc_ht=scontent.fvga2-1.fna&tp=6&oh=16e046a99e6983f9b20acf7eef73d74f&oe=5F6F5F55'
         profileSrc='http://www.gravatar.com/avatar/e1417e55e634ab81b46a2b75ea6d3793?s=200&r=pg&default=mm'
         title='subash penneru'
       />
